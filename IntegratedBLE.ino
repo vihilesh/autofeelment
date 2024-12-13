19b1565c6fef02e29d471de7a3b4433d1988a6ac
Sundar Veliah <sundarveliah@mac-mini.lan>
2024-12-12 21:51:46 -0800

    changes made at home

--- IntegratedBLE.ino
+++ IntegratedBLE.ino
@@ -1,4 +1,5 @@
 /*
+ /*
     Video: https://www.youtube.com/watch?v=oCMOYS71NIU
     Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleNotify.cpp
     Ported to Arduino ESP32 by Evandro Copercini
@@ -19,10 +20,22 @@
    In this example rxValue is the data received (only accessible inside that function).
    And txValue is the data to be sent, in this example just a byte incremented every second. 
 */
+#include <string.h>
+#include <Arduino.h>
+#include <SPI.h>
 #include <BLEDevice.h>
 #include <BLEServer.h>
 #include <BLEUtils.h>
 #include <BLE2902.h>
+
+
+#include <PololuMaestro.h>
+
+  
+
+
+
+
 
 BLECharacteristic *pCharacteristic;
 bool deviceConnected = false;
@@ -39,6 +52,13 @@ const int LED = 2; // Could be different depending on the dev board. I used the 
 #define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
 #define CHARACTERISTIC_UUID_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
 
+// Create Maestro controller instance using Serial1 
+// (typically on pins 18/19 for Mega-based Arduinos)
+//MaestroServoController2 maestro();
+
+MiniMaestro maestro(Serial1);
+
+
 class MyServerCallbacks: public BLEServerCallbacks {
     void onConnect(BLEServer* pServer) {
       deviceConnected = true;
@@ -50,49 +70,102 @@ class MyServerCallbacks: public BLEServerCallbacks {
 };
 
 class MyCallbacks: public BLECharacteristicCallbacks {
-    void onWrite(BLECharacteristic *pCharacteristic) {
+      void onWrite(BLECharacteristic *pCharacteristic) {
      
-      std::string rxValue = pCharacteristic->getValue().c_str();
+        std::string rxValue = pCharacteristic->getValue().c_str();
 
-      if (rxValue.length() > 0) {
-        Serial.println("*********");
-        Serial.print("Received Value: ");
+        if (rxValue.length() > 0) {
+          Serial.println("*********");
+          Serial.print("Received Value: ");
+          Serial.println(rxValue.c_str());
 
-        boolean pressed = false;
-        uint8_t buttnum = 0;
-        for (int i = 0; i < rxValue.length(); i++) {
-          if (rxValue[1] == 'B') {
-            buttnum = rxValue[2] - '0';
-            boolean pressed = rxValue[3] - '0';
+          boolean pressed = false;
+          uint8_t buttnum = 0;
+          for (int i = 0; i < rxValue.length(); i++) {
+            if (rxValue[1] == 'B') {
+              buttnum = rxValue[2] - '0';
+              boolean pressed = rxValue[3] - '0';
+            }
           }
+
+          //Serial.println();
+
+          // Do stuff based on the command received from the app
+          Serial.print("Buttnum: "); 
+          Serial.println(buttnum);
+          Serial.print("Pressed: ");
+          Serial.println(pressed);
+       
+
+          if ((buttnum == 1) ) {
+            Serial.println("Turning 1");
+            digitalWrite(LED, LOW);
+
+             maestro.setSpeed(0, 0);
+             maestro.setAcceleration(0, 0);
+            for (unsigned short pos = 0; pos <= 1000; pos += 500) {
+              maestro.setTarget(0, pos);
+              delay(200);
+            }
+            delay(2000);
+            // maestro.setTarget(0,8000);
+            // delay(2000);
+            //while(true);
+            Serial.println("Loop done");
+          
+            // maestro.setSpeed(1, 0);
+            // maestro.setAcceleration(1, 0);
+            // maestro.setTarget(1,4000);
+            // delay(2000);
+            // maestro.setTarget(1,8000);
+            // delay(2000);
+            // //while(true);
+            // Serial.println("Loop done");
+          
+            // maestro.setSpeed(2, 0);
+            // maestro.setAcceleration(2, 0);
+            // maestro.setTarget(2,4000);
+            // delay(2000);
+            // maestro.setTarget(2,8000);
+            // delay(2000);
+            // //while(true);
+            // Serial.println("Loop done");
+          
+            // maestro.setSpeed(3, 0);
+            // maestro.setAcceleration(3, 0);
+            // maestro.setTarget(3,4000);
+            // delay(2000);
+            // maestro.setTarget(3,8000);
+            // delay(2000);
+
+          } else if( buttnum == 2) {
+            Serial.println("Turning 2");
+            digitalWrite(LED, LOW);
+          } else if( buttnum == 3) {
+            Serial.println("Turning 3");
+            digitalWrite(LED, LOW);
+          } else if( buttnum == 4) {
+            Serial.println("Turning 4");
+            digitalWrite(LED, LOW);
+          }
+
+          Serial.println();
+          Serial.println("*********");
+          pressed = false;
         }
-
-        //Serial.println();
-
-        // Do stuff based on the command received from the app
-        
-        if (buttnum == 1 ) {
-          Serial.println("Turning 1");
-          digitalWrite(LED, LOW);
-        } else if( buttnum == 2) {
-          Serial.println("Turning 2");
-          digitalWrite(LED, LOW);
-        } else if( buttnum == 3) {
-          Serial.println("Turning 3");
-          digitalWrite(LED, LOW);
-        } else if( buttnum == 4) {
-          Serial.println("Turning 4");
-          digitalWrite(LED, LOW);
-        }
-
-        Serial.println();
-        Serial.println("*********");
+       
       }
-    }
 };
 
 void setup() {
-  Serial.begin(115200);
+  
+  Serial1.begin(115200);
+  Serial.begin(9600);
+  Serial.println("Pololu Maestro Servo Controller Initialized");
+  maestro.goHome();
+  //Serial.println("Maestro at Home");
+  //maestro.setTarget(0, 5000);
+  delay(1000);
 
   pinMode(LED, OUTPUT);
 
@@ -131,6 +204,8 @@ void setup() {
 
 void loop() {
   if (deviceConnected) {
+    maestro.setTarget(0, 5000);
+
     // Fabricate some arbitrary junk for now...
     txValue = analogRead(readPin) / 3.456; // This could be an actual sensor reading!
 
@@ -142,10 +217,10 @@ void loop() {
 //    pCharacteristic->setValue("Hello!"); // Sending a test message
     pCharacteristic->setValue(txString);
     
-    pCharacteristic->notify(); // Send the value to the app!
-    Serial.print("*** Sent Value: ");
-    Serial.print(txString);
-    Serial.println(" ***");
+   // pCharacteristic->notify(); // Send the value to the app!
+    // Serial.print("*** Sent Value: ");
+    // Serial.print(txString);
+    // Serial.println(" ***");
 
     // You can add the rxValue checks down here instead
     // if you set "rxValue" as a global var at the top!
